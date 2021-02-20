fa07e5236cd95e82279b2fd6c7391588bb50a0ee
AbhijithMogaveera <abhialur8898@gmail.com>
2021-02-20 13:55:19 +0530

    init commit

---
+++ app/src/main/java/com/abhijith/networkcaching/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.abhijith.networkcaching.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abhijith.networkcaching.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
